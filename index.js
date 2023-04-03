8af6bf0bfe6ed3161cd9710b41de60dc6b11a07a
2100031569 <110450684+kunamnenijyothirmyee@users.noreply.github.com>
2023-04-03 12:21:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
-const app = require('./app')
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const server = http.createServer(app)
-const PORT = config.PORT
-server.listen(PORT, () => 
-{
-  logger.info(`Server running on port ${PORT}`)
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+
+const sequelize = new Sequelize(`${process.env.DB_PSQL_URL}`, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
+
+class Blogs extends Model {}
+
+Blogs.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.STRING,
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        default: 0
+    }
+}, {
+    sequelize
+})
+
+Blogs.sync()
+
+app.use(express.json())
+
+app.get('/api/blogs', async(req, res) => {
+    try {
+        const blogs = await Blogs.findAll()
+        console.log(JSON.stringify(blogs, null, 2))
+        res.status(200).json(blogs)
+    } catch(err) {
+        console.log(err)
+        res.status(400).json({ msg: err })
+    }
+})
+
+app.post('/api/blogs', async(req, res) => {
+    try {
+        const blog = req.body
+        const createBlogs = await Blogs.create(blog)
+        return res.status(201).json(createBlogs)
+    } catch(err) {
+        console.log(err)
+        return res.status(400).json({ msg: err })
+    }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+    const blogId = req.params.id
+    try {
+        const blog = await Blogs.findByPk(blogId)
+        await blog.destroy()
+        res.status(200).json(blog)
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json({ msg: err })
+    }
+})
+
+const PORT = 3005
+
+app.listen(PORT, () => {
+    console.log(`Listening in PORT: ${PORT}!`)
 })
